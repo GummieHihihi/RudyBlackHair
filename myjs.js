b516dc06a280a25ab685365e6082cce88284180d
DuongPham <58169187+GummieHihihi@users.noreply.github.com>
2020-02-19 09:44:13 +0700

    19/2/2020

--- myjs.js
+++ myjs.js
@@ -1,5 +1,40 @@
 
 
+const text = document.getElementById("input").value;
+var like = 0;
+var j =0;
+var input = document.getElementById("input");
+var array = [];
+const resultprint = document.getElementById("resultprint");
+
+input.addEventListener('input', function() {
+	inputValue = document.getElementById("input").value;
+	console.log("the character input is : " + inputValue);
+	$.getJSON( "data.json", function( data ) {
+
+		console.log(data);
+		for(var i =0; i < data.length-1; i ++){
+			console.log(data);
+			if(data[i].name.charAt(0).toUpperCase() == inputValue.charAt(0).toUpperCase()){
+				array[j] = data[i];
+				j = j+1;
+			}
+		}
+		console.log(array);
+		printresult(array);
+	});
+});
+
+function printresult( array){
+	for(var j =0; j < array.length;j++){
+		resultprint.innerHTML =  "name : " + array[j].name +  " , " + " abbr : " 
+		+ array[j].abbr;
+
+	}
+}
+
+
+
 var turn1 =0;
 var turn2 =0;
 var turn3 =0;
@@ -20,16 +55,21 @@ const displaypayment = document.getElementById("displaypayment");
 
 contact.onclick = function(){
 	if(turn1 %2 == 0){
-	displaycontact.innerHTML = "<b>Sale hotline</b>" + " +6592177659" 
-	+ "<br>" + "<b>Adress</b>" + "Upper Serangoon road, Upper " +"<br>" +
-	" Serangoon Shopping center #01-21, Singapore 534626 " + "<br>"
-	+"Monday to Sunday" + "11a.m to 8p.m" + "<br><b>Contact web page</b>" + "<br><b>Location Map </b>"
-	+"<b>about us</b>";
+		displaycontact.innerHTML = "<b>Sale hotline</b>" + " +6592177659" 
+		+ "<br>" + "<b>Adress</b>" + "Upper Serangoon road, Upper " +"<br>" +
+		" Serangoon Shopping center #01-21, Singapore 534626 " + "<br>"
+		+"Monday to Sunday" + "11a.m to 8p.m" + "<br><b>Contact web page</b>" + "<br><b>Location Map </b>"
+		+"<b>about us</b>";
 		document.getElementById("displaycontact").style.display = "block";
 		document.getElementById("displayFAQs").style.display = "none";
 		document.getElementById("displaydelivery").style.display = "none";
 		document.getElementById("displayguidepage").style.display = "none";
 		document.getElementById("displaypayment").style.display = "none";
+		turn1 = 0;
+		turn2 = 0;
+		turn3 = 0;
+		turn4 = 0;
+		turn5 = 0;
 	}
 	else{
 		document.getElementById("displaycontact").style.display = "none";
@@ -43,11 +83,16 @@ contact.onclick = function(){
 FAQs.onclick = function(){
 	if(turn2 %2 == 0){
 		displayFAQs.innerHTML = "aaaaaaaaaaaaaa";
-	document.getElementById("displaycontact").style.display = "none";
+		document.getElementById("displaycontact").style.display = "none";
 		document.getElementById("displayFAQs").style.display = "block";
 		document.getElementById("displaydelivery").style.display = "none";
 		document.getElementById("displayguidepage").style.display = "none";
 		document.getElementById("displaypayment").style.display = "none";
+		turn1 = 0;
+		turn2 = 0;
+		turn3 = 0;
+		turn4 = 0;
+		turn5 = 0;
 	}
 	else{
 		document.getElementById("displaycontact").style.display = "none";
@@ -61,11 +106,16 @@ FAQs.onclick = function(){
 delivery.onclick = function(){
 	if(turn3 %2 == 0){
 		displaydelivery.innerHTML = "aaaaaaaaaaaaaa";
-	document.getElementById("displaycontact").style.display = "none";
+		document.getElementById("displaycontact").style.display = "none";
 		document.getElementById("displayFAQs").style.display = "none";
 		document.getElementById("displaydelivery").style.display = "block";
 		document.getElementById("displayguidepage").style.display = "none";
 		document.getElementById("displaypayment").style.display = "none";
+		turn1 = 0;
+		turn2 = 0;
+		turn3 = 0;
+		turn4 = 0;
+		turn5 = 0;
 	}
 	else{
 		document.getElementById("displaycontact").style.display = "none";
@@ -79,11 +129,16 @@ delivery.onclick = function(){
 guidepage.onclick = function(){
 	if(turn4 %2 == 0){
 		displayguidepage.innerHTML = "aaaaaaaaaaaaaa";
-	document.getElementById("displaycontact").style.display = "none";
+		document.getElementById("displaycontact").style.display = "none";
 		document.getElementById("displayFAQs").style.display = "none";
 		document.getElementById("displaydelivery").style.display = "none";
 		document.getElementById("displayguidepage").style.display = "block";
 		document.getElementById("displaypayment").style.display = "none";
+		turn1 = 0;
+		turn2 = 0;
+		turn3 = 0;
+		turn4 = 0;
+		turn5 = 0;
 	}
 	else{
 		document.getElementById("displaycontact").style.display = "none";
@@ -97,11 +152,16 @@ guidepage.onclick = function(){
 payment.onclick = function(){
 	if(turn5 %2 == 0){
 		displaypayment.innerHTML = "aaaaaaaaaaaaaa";
-	document.getElementById("displaycontact").style.display = "none";
+		document.getElementById("displaycontact").style.display = "none";
 		document.getElementById("displayFAQs").style.display = "none";
 		document.getElementById("displaydelivery").style.display = "none";
 		document.getElementById("displayguidepage").style.display = "none";
 		document.getElementById("displaypayment").style.display = "block";
+		turn1 = 0;
+		turn2 = 0;
+		turn3 = 0;
+		turn4 = 0;
+		turn5 = 0;
 	}
 	else{
 		document.getElementById("displaycontact").style.display = "none";
