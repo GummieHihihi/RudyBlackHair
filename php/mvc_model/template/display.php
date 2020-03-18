51d3396860dfbe31d72fbb70ac11b0f83eb2f172
DuongPham <58169187+GummieHihihi@users.noreply.github.com>
2020-03-18 11:53:53 +0700

    18/03/2020

---
+++ php/mvc_model/template/display.php
@@ -0,0 +1,38 @@
+<?php 
+$row = $_SESSION['row'];
+?>
+
+<center>
+	<table class="table" style="margin: auto" >
+		<thead>
+			<tr>
+				<th class="col">ID</th>
+				<th class="col">Name</th>
+				<th class="col">Price</th>
+				<th class="col">image</th>
+			</tr>
+		</thead>
+		<?php
+		foreach($row as $value) {
+			$productId = $value[0];
+			$productName = $value[1];
+			$productStatus = $value[2];
+			$Image = $value[3];
+			?>
+			<tbody>
+				<tr>
+					<td><?php echo $productId; ?></td>
+					<td><?php echo $productName; ?></td>
+					<td><?php echo $productStatus; ?></td>
+					<td><img class = "img-respondsive" style="width:300px;height:300px;"src="../picture/<?php echo $Image; ?>"></td>
+					<form method = "post" action= "../product/editrequest_<?php  echo $productId;?>">
+						<td><button class="btn btn-primary" name = "editThis" > Edit </button></td>
+					</form>
+					<form method = "post" action= "../product/delete_<?php  echo $productId;?>">
+						<td><button class="btn btn-primary" name = "deleteThis" >Delete</button></td>
+					</form>
+				</tr>
+			</tbody>
+		<?php } ?>
+	</table>
+</center>
---
+++ php/mvc_model/template/template.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * 
+ */
+class Template
+{
+	
+	function __construct()
+	{
+	}
+	public function display($row){
+		$_SESSION['row'] = $row;
+		include_once('../template/display.php');
+	}
+	public function editform_layout($id){
+		include('../template/edit.php');
+	}
+	public function search_layout(){
+		include('../template/search.php');
+	}
+	public function add_layout(){
+		include('../template/add.php');
+	}
+	public function display_Frontend($row){
+		header('C:/xampp/htdocs/duong/php/mvc_model/front_end/demo_withProperties.php');
+	}
+}
+
+?>
